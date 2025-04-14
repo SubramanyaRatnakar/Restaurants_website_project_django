a6e8d84216ff5e889c73f3b4bcbd41b4d849ed1c
Subramanya Ratnakar <suppu@Subramanyas-MacBook-Air.local>
2025-04-14 12:38:23 +0530

    recent

--- restaurants_website_project_django/settings.py
+++ restaurants_website_project_django/settings.py
@@ -139,4 +139,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS=['Base_App.onrender.com']
+ALLOWED_HOSTS=['restaurants-website-project-django-2.onrender.com']
